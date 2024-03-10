e0a982f964ff2f1d5b8e9f64b3b89b7fb2c66e8a
seyifunmi Odediran <philipoluseyi@gmail.com>
2024-03-10 19:24:25 +0100

    1962

--- frontend/src/Components/VideoPage/VideoPage.js
+++ frontend/src/Components/VideoPage/VideoPage.js
@@ -4,6 +4,7 @@ import './VideoPageStyle.css'
 import Footer from "../Footer/Footer";
 import ReactPlayer from 'react-player';
 import { useParams } from 'react-router-dom';
+import {Helmet} from "react-helmet-async";
 
 export default function VideoPage(){
     const { videoId } = useParams();
@@ -18,6 +19,33 @@ export default function VideoPage(){
 
     return (
         <>
+            <Helmet>
+                <title>Home Page</title>
+                <meta
+                    name='description'
+                    content='Unlock your potential with our programming courses. From beginner to advanced levels, master the art of programming with our expert-led courses.'
+                />
+                <meta property="og:title" content="Starpenzu" />
+
+                <meta name="keywords" content="Programming, CodingCourses, TechEducation, tech, computer programming courses" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://yourwebsite.com" />
+                <meta property="og:image" content="https://example.com/image.jpg" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Your Website Title" />
+                <meta
+                    name="twitter:description"
+                    content="Embark on a coding journey with our premier programming courses! From Python to JavaScript, level up your skills with expert-led tutorials and hands-on projects."
+                />
+                <meta name="twitter:image" content="https://example.com/image.jpg" />
+                <meta name="twitter:site" content="@yourtwitterhandle" />
+                <meta
+                    property="og:description"
+                    content="Embark on a coding journey with our premier programming courses! From Python to JavaScript, level up your skills with expert-led tutorials and hands-on projects."
+                />
+                <link rel="canonical" href="https://starpenzu-seo.netlify.app/video/:videoId/:videoTitle"/>
+
+            </Helmet>
             <NavBar/>
             <div className="videoPageCont">
                 <div className="videoPageContHeader">
